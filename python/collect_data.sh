4581d809f58ca492b9cfa03e9e6fbd7af1a9885b
Paolo Lungaroni <paolo.lungaroni@uniroma2.it>
2025-04-10 00:01:35 +0200

    update udp_test.sh and collect_data.sh

--- python/collect_data.sh
+++ python/collect_data.sh
@@ -9,4 +9,4 @@ ssh -i /home/smarts/tmp-key tempuser@139.28.149.89 "mkdir -p ${REMOTE_DIR_PATH}"
 # Transfer the file
 scp -i /home/smarts/tmp-key -r /root/data/*.json tempuser@139.28.149.89:${REMOTE_DIR_PATH}
 sleep 1
-scp -i /home/smarts/tmp-key -r /root/data/*.pcap tempuser@139.28.149.89:${REMOTE_DIR_PATH}
+#scp -i /home/smarts/tmp-key -r /root/data/*.pcap tempuser@139.28.149.89:${REMOTE_DIR_PATH}
